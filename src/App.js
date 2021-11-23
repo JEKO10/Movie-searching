338b42c14730edd3915d91824970895dfd23a316
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-23 18:06:43 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.omdbapi.com/?apikey=a931948e&s=batman"
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <div>
       <h1>Movie searching</h1>
